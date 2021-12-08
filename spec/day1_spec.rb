ad26c47cc94b909c502d486e0f2e934471625b9a
Keith Wedinger <keith@emporatitle.com>
2021-12-08 12:58:58 -0500

    Remove spec input data duplication

--- spec/day1_spec.rb
+++ spec/day1_spec.rb
@@ -6,36 +6,30 @@ require 'utils'
 describe Day1 do
   let(:day1) { described_class.new }
 
+  let(:example_input) do
+    [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+  end
+
+  let(:file_input) { Utils.read_numbers_from(file: 'input/day1.txt') }
+
   describe '#part_one' do
     context 'with example input' do
-      let(:depths) do
-        [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-      end
-
-      it { expect(day1.part_one(depths: depths)).to eq(7) }
+      it { expect(day1.part_one(depths: example_input)).to eq(7) }
     end
 
     context 'with puzzle input file' do
-      let(:depths) { Utils.read_numbers_from(file: 'input/day1.txt') }
-
-      it { expect(day1.part_one(depths: depths)).to eq(1162) }
+      it { expect(day1.part_one(depths: file_input)).to eq(1162) }
     end
 
   end
 
   describe '#part_two' do
     context 'with example input' do
-      let(:depths) do
-        [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-      end
-
-      it { expect(day1.part_two(depths: depths)).to eq(5) }
+      it { expect(day1.part_two(depths: example_input)).to eq(5) }
     end
 
     context 'with puzzle input file' do
-      let(:depths) { Utils.read_numbers_from(file: 'input/day1.txt') }
-
-      it { expect(day1.part_two(depths: depths)).to eq(1190) }
+      it { expect(day1.part_two(depths: file_input)).to eq(1190) }
     end
   end
 end
--- spec/day2_spec.rb
+++ spec/day2_spec.rb
@@ -6,49 +6,38 @@ require 'utils'
 describe Day2 do
   let(:day2) { described_class.new }
 
+  let(:example_input) do
+    [
+      'forward 5',
+      'down 5',
+      'forward 8',
+      'up 3',
+      'down 8',
+      'forward 2'
+    ]
+  end
+
+  let(:file_input) { File.readlines('input/day2.txt') }
+
   describe '#part_one' do
     context 'with example input' do
-      let(:commands) do
-        [
-          'forward 5',
-          'down 5',
-          'forward 8',
-          'up 3',
-          'down 8',
-          'forward 2'
-        ]
-      end
-
-      it { expect(day2.part_one(commands: commands)).to eq(150) }
+      it { expect(day2.part_one(commands: example_input)).to eq(150) }
     end
 
     context 'with puzzle input file' do
-      let(:commands) { File.readlines('input/day2.txt') }
-
-      it { expect(day2.part_one(commands: commands)).to eq(1_488_669) }
+      it { expect(day2.part_one(commands: file_input)).to eq(1_488_669) }
     end
   end
 
   describe '#part_two' do
     context 'with example input' do
-      let(:commands) do
-        [
-          'forward 5',
-          'down 5',
-          'forward 8',
-          'up 3',
-          'down 8',
-          'forward 2'
-        ]
-      end
-
-      it { expect(day2.part_two(commands: commands)).to eq(900) }
+      it { expect(day2.part_two(commands: example_input)).to eq(900) }
     end
 
     context 'with puzzle input file' do
       let(:commands) { File.readlines('input/day2.txt') }
 
-      it { expect(day2.part_two(commands: commands)).to eq(1_176_514_794) }
+      it { expect(day2.part_two(commands: file_input)).to eq(1_176_514_794) }
     end
   end
 end
